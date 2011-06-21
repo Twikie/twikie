8c33a5b395306cd8fe0c29b06bcd6e6afb96f680
Kalon <kh.cubed@gmail.com>
2011-06-21 00:24:47 -0400

    fixed import typo

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.auth.models import User
+from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.ForeignKey(User, unique=True)
