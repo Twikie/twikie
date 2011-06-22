5572aba35357cfcd0e7ffa758ddc4c91cc98ad8d
Kalon <kh.cubed@gmail.com>
2011-06-22 00:56:41 -0400

    addition of various profile fields

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.ForeignKey(User, unique=True)
+    url = models.URLField(blank=True)
+    company = models.CharField(max_length=100, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    is_hireable = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.user.username
