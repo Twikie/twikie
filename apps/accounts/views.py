f9661fe5f73a75ab37642167b75143d5419355aa
nthomson <nthomsondev@gmail.com>
2011-06-25 00:17:53 -0400

    Update templates to work with base. Create cloud container on user registration

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from accounts.forms import RegistrationForm
 from django.template import RequestContext
 from django.http import  HttpResponse
 from django.contrib.auth.models import User
 from frat.models import Project
+from frat.cloud_handlers import create_cloud_container
 
 def registration(request):
     if request.method == 'POST':
@@ -11,17 +12,18 @@ def registration(request):
         if form.is_valid():
             new_user = form.save()
             new_user.set_password(new_user.password)
+            create_cloud_container ( new_user.username )
             new_user.save()
             return HttpResponse('User created')
     else:
         form = RegistrationForm()
-    return render_to_response('registration.html', {'form': form}, context_instance=RequestContext(request))
+    return render(request, 'registration.html', {'form': form}, context_instance=RequestContext(request))
     
 def users(request):
     users = User.objects.all()
-    return render_to_response('users.html', {'users': users})
+    return render(request, 'users.html', {'users': users})
     
 def profile(request, user_name):
     user = User.objects.get(username=user_name);
     projects = Project.objects.filter(owner=user)
-    return render_to_response('profile.html', {'user': user, 'projects': projects} );
+    return render(request, 'profile.html', {'owner': user, 'projects': projects} );
