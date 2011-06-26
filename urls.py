74e9f775fcc242fd7540b2a93368d04003c51243
nthomson <nthomsondev@gmail.com>
2011-06-26 01:20:05 -0400

    Add static file, add annotate

--- urls.py
+++ urls.py
@@ -1,4 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
+
+from django.conf import settings
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -27,3 +31,14 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+
+
+#BAD.
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
+
