14a9a563b7d27449d1f4d2c0ed3968e233bb19c8
nthomson <nthomsondev@gmail.com>
2011-07-03 23:40:08 -0400

    Various fixes, new gravatar stuff, update frat

--- apps/lib/templatetags/gravatar.py
+++ apps/lib/templatetags/gravatar.py
@@ -14,29 +14,32 @@ import urllib, hashlib
 register = template.Library()
 
 class GravatarUrlNode(template.Node):
-    def __init__(self, email):
+    def __init__(self, email, size):
         self.email = template.Variable(email)
+        self.size = template.Variable(size)
 
     def render(self, context):
         try:
             email = self.email.resolve(context)
+            size = self.size.resolve(context)
         except template.VariableDoesNotExist:
             return ''
 
         default = "mm"
-        size = 64
+        
 
         gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
         gravatar_url += urllib.urlencode({'d':default, 's':str(size)})
+        
+        image_tag = "<img src='%s' class='gravatar' />" % gravatar_url
 
-        return gravatar_url
+        return image_tag
 
 @register.tag
-def gravatar_url(parser, token):
+def gravatar_img(parser, token):
     try:
-        tag_name, email = token.split_contents()
-
+        tag_name, email, size = token.split_contents()
     except ValueError:
         raise template.TemplateSyntaxError, "%r tag requires a single argument" % token.contents.split()[0]
 
-    return GravatarUrlNode(email)
+    return GravatarUrlNode(email, size)
